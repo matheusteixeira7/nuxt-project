06dd4aae03a573f357a47b08cfbe3bccfb344e4d
Matheus Teixeira <matheus.teixeira7@gmail.com>
2023-04-07 22:02:09 -0300

    chore: refactor post list template to improve SEO

--- src/components/PostCard.vue
+++ src/components/PostCard.vue
@@ -1,36 +1,33 @@
 <template>
-  <div
-      v-for="(post, postIndex) in posts"
-      :key="postIndex"
-      class="bg-inherit transition ease-in hover:bg-neutral-800 rounded-2xl lg:max-w-xl px-4 md:px-12 py-6 md:py-4 "
-    >
-    <a href="/">
-      <div class="flex items-center mb-4">
-        <div class="h-4 w-0.5 rounded-full bg-zinc-200 dark:bg-zinc-500" />
-        <span class="pl-3.5 text-sm font-medium leading-relaxed text-zinc-500">{{
-          new Intl.DateTimeFormat("en-US", {
-            month: "long",
-            day: "numeric",
-            year: "numeric",
-          }).format(post.date)
-        }}</span>
-      </div>
+  <article
+    v-for="(post, postIndex) in posts"
+    :key="postIndex"
+    class="bg-inherit transition ease-in hover:bg-neutral-800 rounded-2xl lg:max-w-xl px-4 md:px-12 py-6 md:py-4"
+  >
+    <a :href="post.link">
+      <time class="flex items-center mb-4" :datetime="post.date.toISOString()">
+        <div class="h-4 w-0.5 rounded-full bg-zinc-200 dark:bg-zinc-500"></div>
+        <span class="pl-3.5 text-sm font-medium leading-relaxed text-zinc-500">
+          {{
+            new Intl.DateTimeFormat("en-US", {
+              month: "long",
+              day: "numeric",
+              year: "numeric",
+            }).format(post.date)
+          }}
+        </span>
+      </time>
       <div>
-        <h2 class="text-base font-bold leading-relaxed text-white mb-2">
-          {{ post.title }}
-        </h2>
+        <h2 class="text-base font-bold leading-relaxed text-white mb-2">{{ post.title }}</h2>
         <p class="text-sm text-zinc-400">{{ post.content }}</p>
       </div>
       <div class="flex items-center mt-4 text-sm font-medium text-teal-500">
-        <span>
-          Read article
-        </span>
+        <span>Read article</span>
         <Icon name="mdi:chevron-right" />
       </div>
     </a>
-  </div>
+  </article>
 </template>
-
 <script setup lang="ts">
 const posts = [
   {
